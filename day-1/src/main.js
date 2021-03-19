78430d10f8478a91003824721e1d63e47603eb11
clicktravel-jamie <jamie.preston@gmail.com>
2021-03-19 09:52:41 +0000

    Add build script

--- day-1/src/main.js
+++ day-1/src/main.js
@@ -1,11 +1,5 @@
-export default (input) =>  {
-    const numbers = input.split('\n');
-    return numbers.reduce((acc, numA) => {
-        const numB = numbers.find(num =>
-            Number(numA) + Number(num) === 2020
-        );
-        return !acc && numB
-            ? [Number(numA), Number(numB)]
-            : acc;
-    }, null);
-}
+import data from './data';
+import { getCalculationFromNumbers } from './expenses-calculator';
+
+const output = getCalculationFromNumbers(data);
+console.log(output);
