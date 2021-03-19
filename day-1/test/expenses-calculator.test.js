4a7cd53a4f2f72a59c74d996e932dc43f57560b8
clicktravel-jamie <jamie.preston@gmail.com>
2021-03-19 09:25:27 +0000

    Day 1 part 1

--- day-1/test/expenses-calculator.test.js
+++ day-1/test/expenses-calculator.test.js
@@ -1,9 +1,17 @@
 import expensesCalculator from '../src/expenses-calculator';
 
 describe('expenses-calculator', () => {
-    it('should return true', () => {
-        const input = '1234'
-        const result = expensesCalculator(input)
-        expect(result).toBe(true);
-    })
-})
+    describe('Given a simple array of numbers', () => {
+        const input = `1721
+979
+366
+299
+675
+1456`;
+        it('should return the first two numbers which add up to 2020', () => {
+            const result = expensesCalculator(input);
+
+            expect(result).toEqual([1721,299]);
+        });
+    });
+});
